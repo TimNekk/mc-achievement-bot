44bd36764a0d8efcd01c9ca0a5099073da5ac29d
unknown <herew26@gmail.com>
2020-09-15 22:24:21 +0300

    Minor changes

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ async def on_startup(dp):
     from utils.notify_admins import on_startup_notify
     await on_startup_notify(dp)
 
+    from utils.set_bot_commands import set_default_commands
+    await set_default_commands(dp)
+
 
 if __name__ == '__main__':
     from aiogram import executor
--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -8,7 +8,7 @@ from data.config import admins
 async def on_startup_notify(dp: Dispatcher):
     for admin in admins:
         try:
-            await dp.bot.send_message(admin, "Бот Запущен")
+            await dp.bot.send_message(admin, "Бот запущен ✨")
 
         except Exception as err:
             logging.exception(err)
